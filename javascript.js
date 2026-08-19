5361d44535c73628a20e98d274695f068aac7782
PeteTheMeat <175096215+PK-Lambda@users.noreply.github.com>
2026-08-19 22:40:20 +1200

    Added a pseudocode to javascript.js where I outlined 11 key steps.

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// 1. A welcome message greets the user in the terminal. 
+// 2. Game instructions are explained inside the terminal: This is a rock, paper and scissors game where you will play against a highly sophisticated AI agent. You will play 5 rounds; every round you choose rock, paper or scissors while the AI does the same thing in the background. The winner is awarded one point. Note: rock beats scissors but gets covered by paper; paper beats rock but gets cut by scissors; scissors beat paper but get cut by rock. A draw is announced if the same items are selected. To win this game, get the most points out of 5 games. Good luck! 
+// 3. humanScore or computerScore empty variables get declared with 0 values assigned
+// 4. A playGame loop is initiated up until player hasn't plated 5 games.
+// 5. Nested AI logic gets created with a function getComputerChoice where the computer randomly chooses one of the options. Note user won't see this. 
+// 6. Player/human logic gets created with a function getHumanChoice where the user is given options to choose from and makes his/her choice. User types their choice in the terminal; note it won't be case-sensitive.
+// 7. User choice gets compared against AI: rock beats scissors but gets covered by paper; paper beats rock but gets cut by scissors; scissors beat paper but get cut by rock othrwise, a draw is announced if the same items are selected. 
+// 8. The winner is awarded 1 point. humanScore or computerScore
+// 9. Steps 5-8 repeat until 5 games have been played.
+// 10. A message appears with an announcement of the winner, thanking them for playing.
+// 11. End of the game
