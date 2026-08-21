1715c56e51f32a96b6f589eca9eb7306b2c8d539
PeteTheMeat <175096215+PK-Lambda@users.noreply.github.com>
2026-08-21 12:42:32 +1200

    I had to do a full review of the code. I created 3 functions getComuterChoice, getHumanChoice and playRound. playRound is the master function that calls the getComputerChoice and getHumanChoice functions. I also created two const variables: computerSelection and humanSelection, which then become arguments of the playRound function to initiate the whole process.

--- javascript.js
+++ javascript.js
@@ -1,17 +1,20 @@
-// 1. A welcome message greets the user in the terminal
-console.log("Welcome to Rock, Paper and Scissors game.")
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Name: javascript.js                                                                                                  //
+// Purpose: This is a Paper, Rock, Scissors game where user play 5 rounds against AI.                                   //
+// Date: 21.08.26                                                                                                       //
+// Author: petequbit                                                                                                    //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// 2. Game instructions are explained inside the terminal
-console.log("This is a rock, paper and scissors game where you will play against a highly sophisticated AI agent. You will play 5 rounds; every round you choose rock, paper or scissors while the AI does the same thing in the background. The winner is awarded one point. Note: rock beats scissors but gets covered by paper; paper beats rock but gets cut by scissors; scissors beat paper but get cut by rock. A draw is announced if the same items are selected. To win this game, get the most points out of 5 games. Good luck!")
+// 1. A welcome message greets the user in the terminal. Game instructions are explained inside the terminal:
 
-// 3. humanScore or computerScore empty variables get declared with 0 values assigned
-let humanScore = 0;
-let computerScore = 0;
+console.log("Welcome to Rock, Paper and Scissors game.");
+console.log("This is a rock, paper and scissors game where you will play against a highly sophisticated AI agent. You will play 5 rounds; every round you choose rock, paper or scissors while the AI does the same thing in the background. The winner is awarded one point. Note: rock beats scissors but gets covered by paper; paper beats rock but gets cut by scissors; scissors beat paper but get cut by rock. A draw is announced if the same items are selected. To win this game, get the most points out of 5 games. Good luck!");
 
+// 2. Nested AI logic gets created with a function getComputerChoice where the computer randomly chooses 
+// one of the options. Note user won't see this:
 
-// 5. Nested AI logic gets created with a function getComputerChoice where the computer randomly chooses one of the options. Note user won't see this. 
 function getComuterChoice () {
-    computerChoice = Math.floor(Math.random() * 3);
+    let computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice == 0) {
         return computerChoice = 'rock';
     } else if (computerChoice == 1) {
@@ -21,51 +24,59 @@ function getComuterChoice () {
     }
 }
 
-// 6. Player/human logic gets created with a function getHumanChoice where the user is given options 
-// to choose from and makes his/her choice. User types their choice in the terminal; note it won't be 
-// case-sensitive.
+// 3. Player/human logic gets created with a function getHumanChoice where the user is given options to choose
+//  from and makes his/her choice. User types their choice in the terminal; note it won't be case-sensitive:
 
 function getHumanChoice () {
     console.log("Chose one of the options:\n rock \n paper \n scissors");
-    humanChoice = prompt("Enter your choice.. ");
-    return humanChoice.toLowerCase();
+    let humanChoice = prompt("Enter your choice.. ");
+    humanChoice.toLowerCase();
+    return humanChoice;
 }
 
-// 4. Declare score variables
-let computerChoice = 0;
-let humanChoice = 0;
+// 4. humanScore or computerScore empty variables get declared with 0 values assigned:
+
+let humanScore = 0;
+let computerScore = 0;
 
 // 7. User choice gets compared against AI: rock beats scissors but gets covered by paper; paper beats 
 // rock but gets cut by scissors; scissors beat paper but get cut by rock othrwise, a draw is announced 
-// if the same items are selected. The winner is awarded 1 point. humanScore or computerScore.
+// if the same items are selected. The winner is awarded 1 point. humanScore or computerScore:
 
-function playRound (getComuterChoice, getHumanChoice) {
+function playRound (humanChoice, computerChoice) {
     if (humanChoice == 'rock' && computerChoice == 'scissors') {
         humanScore++;
-        return console.log("You won. Rock beats scissors.");
+        console.log(`You won. Rock beats scissors. Your score: ${humanScore} Computer Score: ${computerScore}`);
     } else if (humanChoice == 'scissors' && computerChoice == 'rock') {
         computerScore++;
-        return console.log("You lost. Rock bats scissors.");
+        console.log(`You lost. Rock bats scissors. Your score: ${humanScore} Computer Score: ${computerScore}`);
     } else if (humanChoice == 'paper' && computerChoice == 'rock') {
         humanScore++;
-        return console.log("You won. Paper beats rock.");
+        console.log(`You won. Paper beats rock. Your score: ${humanScore} Computer Score: ${computerScore}`);
     } else if (humanChoice == 'rock' && computerChoice == 'paper') {
         computerScore++;
-        return console.log("You lost. Paper beats rock.");
+        console.log(`You lost. Paper beats rock. Your score: ${humanScore} Computer Score: ${computerScore}`);
     } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
         humanScore++;
-        return console.log("You won. Scissors beats paper.");
+        console.log(`You won. Scissors beats paper.Your score: ${humanScore} Computer Score: ${computerScore}`);
     } else if (humanChoice == 'paper' && computerChoice == 'scissors') {
         computerScore++;
-        return console.log("You lost. Scissors beats paper.");
+        console.log(`You lost. Scissors beats paper. Your score: ${humanScore} Computer Score: ${computerScore}`);
     } else {
         humanScore++;
         computerScore++;
-        return console.log("It's a draw, both players get two points");
+        console.log(`It's a draw, both players get two points. Your score: ${humanScore} Computer Score: ${computerScore}`);
     }
 }
 
+const computerSelection = getComuterChoice();
+const humanSelection = getHumanChoice();
+
+playRound(humanSelection, computerSelection);
+
+// 8. Play game logic 
+
 // 10. A message appears with an announcement of the winner, thanking them for playing.
-    
+
 // 11. End of the game
  
